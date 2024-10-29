90e1544bc6d13e6a8d433d6a386d61707c021f3b
Djordje J <jovanovic.djo@gmail.com>
2024-10-29 13:51:04 +0100

    remove excess datasets and add initial metrics script

---
+++ improving-writing-assistance-at-jetbrains-ai/scripts/metrics.py
@@ -0,0 +1,11 @@
+from nltk.metrics.distance import edit_distance
+
+def calculate_avg_levenshtein(incorrect_sentences, correct_sentences):
+    distances = [edit_distance(incorrect, correct) for incorrect, correct in zip(incorrect_sentences, correct_sentences)]
+    avg_distance = sum(distances) / len(distances)
+    return avg_distance
+
+def calculate_accuracy(predicted_sentences, correct_sentences):
+    correct_matches = sum([1 for predicted, correct in zip(predicted_sentences, correct_sentences) if predicted == correct])
+    accuracy = correct_matches / len(correct_sentences) * 100
+    return accuracy
