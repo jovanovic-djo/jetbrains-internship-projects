03a193c90f28f9aeccd6518483a9a35b923b9a32
Djordje J <jovanovic.djo@gmail.com>
2024-10-30 11:58:12 +0100

    add separated library result generation

--- improving-writing-assistance-at-jetbrains-ai/scripts/result generation/libraries_results_generation.py
+++ improving-writing-assistance-at-jetbrains-ai/scripts/result generation/libraries_results_generation.py
@@ -19,28 +19,160 @@ def generate_merged_libraries_results(input_path, output_path):
         pyspell_result = scl.pyspellchecker_correct(incorrect)
         textblob_result = scl.textblob_correct(incorrect)
         symspell_result = scl.symspell_correct(incorrect)
-        enchant_result = scl.enchant_correct(incorrect)
+        pyenchant_result = scl.pyenchant_correct(incorrect)
 
-        for spell_checker_name, result in [
+        for library_name, result in [
             ("PySpellChecker", pyspell_result),
             ("TextBlob", textblob_result),
             ("SymSpell", symspell_result),
-            ("PyEnchat", enchant_result)]:
-            lev_distance = metrics.calculate_levenshtein(result, correct)
+            ("PyEnchant", pyenchant_result)]:
+            levenshtein_distance = metrics.calculate_levenshtein(result, correct)
             accuracy = metrics.calculate_accuracy(result, correct)
             exact_match = metrics.calculate_exact_match(result, correct)
 
 
             results.append({
                 "Index": index,
-                "SpellChecker": spell_checker_name,
+                "SpellChecker": library_name,
                 "Original": incorrect,
                 "Corrected": correct,
                 "Predicted": result,
-                "Levenshtein Distance": lev_distance,
+                "Levenshtein Distance": levenshtein_distance,
                 "Accuracy": accuracy,
                 "Exact Match": exact_match
             })
 
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(output_path, index=False)
+
+
+
+def generate_pyspellchecker_results(input_path, output_path):
+
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f"Input file not found: {input_path}")
+
+    df = pd.read_csv(input_path, delimiter=";")
+    results = []
+
+    for index, row in df.iterrows():
+        incorrect = row['incorrect']
+        correct = row['correct']
+
+        library_result = scl.pyspellchecker_correct(incorrect)
+
+        levenshtein_distance = metrics.calculate_levenshtein(library_result, correct)
+        accuracy = metrics.calculate_accuracy(library_result, correct)
+        exact_match = metrics.calculate_exact_match(library_result, correct)
+
+        results.append({
+            "Index": index,
+            "Original": incorrect,
+            "Corrected": correct,
+            "Predicted": library_result,
+            "Levenshtein Distance": levenshtein_distance,
+            "Accuracy": accuracy,
+            "Exact Match": exact_match
+        })
+    
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(output_path, index=False)
+
+
+
+def generate_symspell_results(input_path, output_path):
+
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f"Input file not found: {input_path}")
+
+    df = pd.read_csv(input_path, delimiter=";")
+    results = []
+
+    for index, row in df.iterrows():
+        incorrect = row['incorrect']
+        correct = row['correct']
+
+        library_result = scl.symspell_correct(incorrect)
+
+        levenshtein_distance = metrics.calculate_levenshtein(library_result, correct)
+        accuracy = metrics.calculate_accuracy(library_result, correct)
+        exact_match = metrics.calculate_exact_match(library_result, correct)
+
+        results.append({
+            "Index": index,
+            "Original": incorrect,
+            "Corrected": correct,
+            "Predicted": library_result,
+            "Levenshtein Distance": levenshtein_distance,
+            "Accuracy": accuracy,
+            "Exact Match": exact_match
+        })
+    
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(output_path, index=False)
+
+
+
+def generate_pyenchant_results(input_path, output_path):
+
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f"Input file not found: {input_path}")
+
+    df = pd.read_csv(input_path, delimiter=";")
+    results = []
+
+    for index, row in df.iterrows():
+        incorrect = row['incorrect']
+        correct = row['correct']
+
+        library_result = scl.pyenchant_correct(incorrect)
+
+        levenshtein_distance = metrics.calculate_levenshtein(library_result, correct)
+        accuracy = metrics.calculate_accuracy(library_result, correct)
+        exact_match = metrics.calculate_exact_match(library_result, correct)
+
+        results.append({
+            "Index": index,
+            "Original": incorrect,
+            "Corrected": correct,
+            "Predicted": library_result,
+            "Levenshtein Distance": levenshtein_distance,
+            "Accuracy": accuracy,
+            "Exact Match": exact_match
+        })
+    
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(output_path, index=False)
+
+
+
+def generate_textblob_results(input_path, output_path):
+
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f"Input file not found: {input_path}")
+
+    df = pd.read_csv(input_path, delimiter=";")
+    results = []
+
+    for index, row in df.iterrows():
+        incorrect = row['incorrect']
+        correct = row['correct']
+
+        library_result = scl.textblob_correct(incorrect)
+
+        levenshtein_distance = metrics.calculate_levenshtein(library_result, correct)
+        accuracy = metrics.calculate_accuracy(library_result, correct)
+        exact_match = metrics.calculate_exact_match(library_result, correct)
+
+        results.append({
+            "Index": index,
+            "Original": incorrect,
+            "Corrected": correct,
+            "Predicted": library_result,
+            "Levenshtein Distance": levenshtein_distance,
+            "Accuracy": accuracy,
+            "Exact Match": exact_match
+        })
+    
     results_df = pd.DataFrame(results)
     results_df.to_csv(output_path, index=False)
--- improving-writing-assistance-at-jetbrains-ai/scripts/spell_check_libraries.py
+++ improving-writing-assistance-at-jetbrains-ai/scripts/spell_check_libraries.py
@@ -32,7 +32,7 @@ def symspell_correct(sentence):
     return ' '.join(corrected_words)
 
 
-def enchant_correct(sentence):
+def pyenchant_correct(sentence):
     corrected_words = [
         word if enchant_checker.check(word) else (enchant_checker.suggest(word)[0] if enchant_checker.suggest(word) else word)
         for word in sentence.split()
@@ -40,24 +40,3 @@ def enchant_correct(sentence):
     return ' '.join(corrected_words)
 
 
-
-
-
-
-# Test examples :: DELETE LATER
-
-# incorrect_sentences = ["Ths is an exmple.", "He went to the stor.", "Somrbodz tat i used to knoe"]
-# correct_sentences = ["This is an example.", "He went to the store.", "Somebody that I used to know"]
-
-# for incorrect, correct in zip(incorrect_sentences, correct_sentences):
-#     pyspell_result = pyspell_correct(incorrect)
-#     textblob_result = textblob_correct(incorrect)
-#     symspell_result = symspell_correct(incorrect)
-#     enchant_result = enchant_correct(incorrect)
-
-#     print(f"Original: {incorrect}")
-#     print(f"Corrected (PySpellChecker): {pyspell_result}")
-#     print(f"Corrected (TextBlob): {textblob_result}")
-#     print(f"Corrected (SymSpell): {symspell_result}")
-#     print(f"Corrected (Enchant): {enchant_result}")
-#     print(f"Expected Correct: {correct}\n")
